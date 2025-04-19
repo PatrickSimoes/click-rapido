2bd7f31424f6ccd8cb55c5b9e285bb63aceaa491
Patrick Simões <patricksimoes25@gmail.com>
2025-04-19 17:29:05 -0300

    fix: adjustment main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TasksModule } from './tasks/tasks.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TasksModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
+
 export class AppModule { }
