59358973521d1347519158f0a100024e51939b57
Patrick Simões <patricksimoes25@gmail.com>
2025-04-28 21:49:03 -0300

    feat: new dependence swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { setupSwagger } from './common/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,       // remove props não-decoradas
@@ -11,9 +13,14 @@ async function bootstrap() {
       forbidNonWhitelisted: true, // erro se props não listadas
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
 
-  Logger.log(`Server running on http://localhost:${process.env.PORT}`, 'Bootstrap');
+  setupSwagger(app);
+
+  const PORT = process.env.PORT || 3000;
+  
+  await app.listen(PORT);
+
+  Logger.log(`Server running on http://localhost:${PORT}`, 'Bootstrap');
 }
 
 bootstrap();
